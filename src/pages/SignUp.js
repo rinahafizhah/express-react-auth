baa08dbaaaf6e1e1d7979c6125cc162bf69bdfb1
Rina Hafizhah U <rinahafizhah@gmail.com>
2019-01-24 14:49:44 +0700

    routing and state setup

---
+++ src/pages/SignUp.js
@@ -0,0 +1,51 @@
+import React, { Component } from "react";
+
+class SignUp extends Component {
+  state = {
+    name: "",
+    age: 0,
+    email: "",
+    password: ""
+  };
+
+  handleChange = e => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+  render() {
+    return (
+      <form>
+        <input
+          type="text"
+          name="name"
+          placeholder="Name"
+          onChange={this.handleChange}
+          value={this.state.name}
+        />
+        <input
+          type="number"
+          name="age"
+          placeholder="Age"
+          onChange={this.handleChange}
+          value={this.state.age}
+        />
+        <input
+          type="email"
+          name="email"
+          placeholder="Email"
+          onChange={this.handleChange}
+          value={this.state.email}
+        />
+        <input
+          type="password"
+          name="password"
+          placeholder="Password"
+          onChange={this.handleChange}
+          value={this.state.password}
+        />
+        <button type="submit">Submit</button>
+      </form>
+    );
+  }
+}
+
+export default SignUp;
